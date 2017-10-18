b25ec19d4b862ce06053107434b8d5b5e45c0b90
pepe_mbp <jcane86@gmail.com>
2017-10-18 17:05:59 +0100

    style(ignore): ignore gitify-docs from lint

--- gitify-docs.js
+++ gitify-docs.js
@@ -3,15 +3,18 @@ const gh = require('jsdoc-githubify');
 const fs = require('fs');
 
 mutiny(
-  { outdir: './docs/', transform: [gh], rename: (file, dir) => `${file}.gh` },
+  { outdir: './docs/', transform: [gh], rename: file => `${file}.gh` },
   { root: './docs/', fileFilter: 'module*.html' },
 );
 
 fs.readdir('./docs/motor-hat/1.0.0-semanticrelease', (err, files) => {
   files.forEach((file) => {
     if (file.indexOf('.gh')) {
-      fs.rename(`./docs/motor-hat/1.0.0-semanticrelease/${file}`, `./docs/motor-hat/1.0.0-semanticrelease/${file.replace('.gh', '')}`,
-        () => ());
+      fs.rename(
+        `./docs/motor-hat/1.0.0-semanticrelease/${file}`,
+        `./docs/motor-hat/1.0.0-semanticrelease/${file.replace('.gh', '')}`,
+        () => {},
+      );
     }
   });
 });
