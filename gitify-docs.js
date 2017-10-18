b7c0f07f2a197291f709b127542b3274e8fe939c
pepe_mbp <jcane86@gmail.com>
2017-10-18 16:49:03 +0100

    docs(auto): add gitifier

---
+++ gitify-docs.js
@@ -0,0 +1,17 @@
+const mutiny = require('mutiny');
+const gh = require('jsdoc-githubify');
+const fs = require('fs');
+
+mutiny(
+  { outdir: './docs/', transform: [gh], rename: (file, dir) => `${file}.gh` },
+  { root: './docs/', fileFilter: 'module*.html' },
+);
+
+fs.readdir('./docs/motor-hat/1.0.0-semanticrelease', (err, files) => {
+  files.forEach((file) => {
+    if (file.indexOf('.gh')) {
+      fs.rename(`./docs/motor-hat/1.0.0-semanticrelease/${file}`, `./docs/motor-hat/1.0.0-semanticrelease/${file.replace('.gh', '')}`,
+        () => ());
+    }
+  });
+});
